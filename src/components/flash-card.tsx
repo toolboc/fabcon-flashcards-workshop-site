a0c5ccb33ddb83edf87c64d71013fdc25a45a7d2
Alvaro Videla <videlalvaro@gmail.com>
2025-03-20 22:46:32 +0100

    fixes strict errors

--- src/components/flash-card.tsx
+++ src/components/flash-card.tsx
@@ -6,7 +6,80 @@ import { Button } from "@/components/ui/button"
 import { useState } from "react"
 import Link from "next/link"
 
+const getGradient = (topic: string): string => {
+  const gradients: { [key: string]: string } = {
+    "introduction-end-analytics-use-microsoft-fabric": "bg-gradient-to-t from-teal-200 to-teal-700",
+    "use-data-factory-pipelines-fabric": "bg-gradient-to-t from-yellow-300 to-green-800",
+    "use-apache-spark-work-files-lakehouse": "bg-gradient-to-t from-yellow-500 to-red-500",
+    "get-started-data-warehouse": "bg-gradient-to-t from-blue-800 to-blue-400",
+    "use-dataflow-gen-2-fabric": "bg-gradient-to-t from-lime-300 to-green-700",
+    "work-delta-lake-tables-fabric": "bg-gradient-to-t from-green-500 to-blue-500",
+    "explore-event-streams-microsoft-fabric": "bg-gradient-to-t from-sky-800 to-blue-800",
+    "administer-fabric": "bg-gradient-to-t from-gray-400 to-black",
+    "ingest-data-with-spark-fabric-notebooks": "bg-gradient-to-t from-slate-300 to-green-800",
+    "get-started-lakehouses": "bg-gradient-to-t from-sky-200 to-sky-700",
+    "describe-medallion-architecture": "bg-gradient-to-t from-yellow-500 to-teal-500",
+    "get-started-kusto-fabric": "bg-gradient-to-t from-teal-600 to-blue-800",
+  }
+
+  return gradients[topic]
+}
+
+const getCardTitle = (topic: string): string => {
+  const cardTitles: { [key: string]: string } = {
+    "introduction-end-analytics-use-microsoft-fabric": "Fabric",
+    "get-started-lakehouses": "Lakehouse",
+    "use-apache-spark-work-files-lakehouse": "Data Engineering",
+    "work-delta-lake-tables-fabric": "Delta Lake",
+    "use-data-factory-pipelines-fabric": "Data Factory",
+    "use-dataflow-gen-2-fabric": "Dataflow Gen2",
+    "get-started-data-warehouse": "Data Warehouse",
+    "administer-fabric": "Fabric Administration",
+    "describe-medallion-architecture": "Medallion Architecture",
+    "ingest-data-with-spark-fabric-notebooks": "Fabric Notebooks",
+    "get-started-kusto-fabric": "Real-Time Intelligence",
+    "explore-event-streams-microsoft-fabric": "Eventstream",
+  }
+
+  return cardTitles[topic]
+}
+
 export function FlashCard({QandAs, topic, moduleTitle}: {QandAs: any, topic: string, moduleTitle: string}) {
+  const [isFlipped, setIsFlipped] = useState(false)
+  const [currentQA, setCurrentQA] = useState(0)
+
+  function flipCard() {
+    setIsFlipped(!isFlipped)
+  }
+
+  function flipToShowQuestion() {
+    setIsFlipped(false)
+  }
+
+  async function nextCard() {
+    const index = currentQA
+    const nextIndex = (index + 1) % QAs.length
+    flipToShowQuestion()
+    await delay(200);
+    setCurrentQA(nextIndex)
+  }
+
+  async function previousCard() {
+    const index = currentQA
+    const previousIndex = (index - 1 + QAs.length) % QAs.length
+    flipToShowQuestion()
+    await delay(200);
+    setCurrentQA(previousIndex)
+  }
+
+  function delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  // Sample Link: https://learn.microsoft.com/training/modules/ingest-data-with-spark-fabric-notebooks/
+  function createLearnLink(unit: string) {
+    return "https://learn.microsoft.com/training/modules/" + topic + "/" + unit + "?WT.mc_id=data-111905-alvidela";
+  }
 
   if (QandAs === undefined || QandAs === null || QandAs[topic] === undefined || QandAs[topic] === null) {
     return (
@@ -18,84 +91,11 @@ export function FlashCard({QandAs, topic, moduleTitle}: {QandAs: any, topic: str
     )
   } else {
     const QAs = QandAs[topic]
-    const [isFlipped, setIsFlipped] = useState(false)
-    const [currentQA, setCurrentQA] = useState(0)
-
-    const gradients: { [key: string]: string } = {
-      "introduction-end-analytics-use-microsoft-fabric": "bg-gradient-to-t from-teal-200 to-teal-700",
-      "use-data-factory-pipelines-fabric": "bg-gradient-to-t from-yellow-300 to-green-800",
-      "use-apache-spark-work-files-lakehouse": "bg-gradient-to-t from-yellow-500 to-red-500",
-      "get-started-data-warehouse": "bg-gradient-to-t from-blue-800 to-blue-400",
-      "use-dataflow-gen-2-fabric": "bg-gradient-to-t from-lime-300 to-green-700",
-      "work-delta-lake-tables-fabric": "bg-gradient-to-t from-green-500 to-blue-500",
-      "explore-event-streams-microsoft-fabric": "bg-gradient-to-t from-sky-800 to-blue-800",
-      "administer-fabric": "bg-gradient-to-t from-gray-400 to-black",
-      "ingest-data-with-spark-fabric-notebooks": "bg-gradient-to-t from-slate-300 to-green-800",
-      "get-started-lakehouses": "bg-gradient-to-t from-sky-200 to-sky-700",
-      "describe-medallion-architecture": "bg-gradient-to-t from-yellow-500 to-teal-500",
-      "get-started-kusto-fabric": "bg-gradient-to-t from-teal-600 to-blue-800",
-    }
-  
-    function getGradient() {
-      return gradients[topic];
-    }
-  
-    const cardTitles: { [key: string]: string } = {
-      "introduction-end-analytics-use-microsoft-fabric": "Fabric",
-      "get-started-lakehouses": "Lakehouse",
-      "use-apache-spark-work-files-lakehouse": "Data Engineering",
-      "work-delta-lake-tables-fabric": "Delta Lake",
-      "use-data-factory-pipelines-fabric": "Data Factory",
-      "use-dataflow-gen-2-fabric": "Dataflow Gen2",
-      "get-started-data-warehouse": "Data Warehouse",
-      "administer-fabric": "Fabric Administration",
-      "describe-medallion-architecture": "Medallion Architecture",
-      "ingest-data-with-spark-fabric-notebooks": "Fabric Notebooks",
-      "get-started-kusto-fabric": "Real-Time Intelligence",
-      "explore-event-streams-microsoft-fabric": "Eventstream",
-    }
-  
-    function getCardTitle() {
-      return cardTitles[topic];
-    }
-  
-    function flipCard() {
-      setIsFlipped(!isFlipped)
-    }
-  
-    function flipToShowQuestion() {
-      setIsFlipped(false)
-    }
-  
-    async function nextCard() {
-      const index = currentQA
-      const nextIndex = (index + 1) % QAs.length
-      flipToShowQuestion()
-      await delay(200);
-      setCurrentQA(nextIndex)
-    }
-  
-    async function previousCard() {
-      const index = currentQA
-      const previousIndex = (index - 1 + QAs.length) % QAs.length
-      flipToShowQuestion()
-      await delay(200);
-      setCurrentQA(previousIndex)
-    }
-  
-    function delay(ms: number) {
-      return new Promise( resolve => setTimeout(resolve, ms) );
-    }
-  
-    // Sample Link: https://learn.microsoft.com/training/modules/ingest-data-with-spark-fabric-notebooks/
-    function createLearnLink(unit: string) {
-      return "https://learn.microsoft.com/training/modules/" + topic + "/" + unit + "?WT.mc_id=data-111905-alvidela";
-    }
   
     return (
       <div id="flashcard" className="flex justify-center items-center h-screen bg-gray-100 dark:bg-gray-900">
-        <Card className={`w-96 rounded-xl shadow-md overflow-hidden m-4 text-white ${getGradient()}`}>
-          <h1 className="text-lg m-5 uppercase">{getCardTitle()}</h1>
+        <Card className={`w-96 rounded-xl shadow-md overflow-hidden m-4 text-white ${getGradient(topic)}`}>
+          <h1 className="text-lg m-5 uppercase">{getCardTitle(topic)}</h1>
           <ReactCardFlip isFlipped={isFlipped}>
             <div className="react-card-front">
               <div className="pb-8">
