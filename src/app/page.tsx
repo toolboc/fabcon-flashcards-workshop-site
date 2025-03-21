7d0c5f5035cebbaf1308f0a41266f749e6346197
Alvaro Videla <videlalvaro@gmail.com>
2025-03-21 19:02:42 +0100

    Update page.tsx to load topics from storage account

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,11 @@ import { Topic } from '@/components/topic'
 import { useState } from "react"
 import { useEffect } from "react"
 
-const topicsURL = "https://fabconworkshopalvidela.blob.core.windows.net/$web/topics.json"
+// const topicsURL = "https://fabconworkshopalvidela.blob.core.windows.net/$web/topics.json"
+const topicsURL = "https://workshoptestst.blob.core.windows.net/$web/topics.json"
 // const topicsURL = "https://raw.githubusercontent.com/videlalvaro/fabcon-flashcards-workshop-site/refs/heads/main/src/topics.json"
 const QAsURL = "https://fabconworkshopalvidela.blob.core.windows.net/$web/generated-QAs.json"
+
 // const QAsURL = "https://raw.githubusercontent.com/videlalvaro/fabcon-flashcards-workshop-site/refs/heads/main/src/generated-QAs.json"
 
 const startingTopic = "get-started-lakehouses"
